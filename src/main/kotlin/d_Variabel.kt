8e5b3a09ccd039c0fe0f37c9f01acfa606647839
Randa Kurnialis <randakurnialis@gmail.com>
2022-03-10 14:32:22 +0700

    Penerapan Variabel di kotlin

---
+++ src/main/kotlin/d_Variabel.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+}
+//*Variabel adalah sebuah tempat/wadah didalam memori komputer untuk menyimpan data seperti
+//data nama pengguna, kata sandi, data waktu, jumlah makanan dan lain-lain sebagainya. Tempat
+//yang digunakan oleh suatu variabel memiliki ukuran yang berbeda-beda didalam memori
+//komputer tergantung dari jenis tipe data yang digunakan pada sebuah variabel.
